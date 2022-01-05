8a1cd234935526c9e668f1f31b0927c903902c5f
蒲龙山 <longshan.pu@kalendsoft.com>
2022-01-05 17:23:29 +0800

    引入连接pg数据库模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    // TypeOrmModule.forRoot({
+    //   type:'postgres',
+    //   host:'1.116.37.473',
+    //   port:9700,
+    //   username: 'postgres',
+    //   password: 'AAaa123456789',
+    //   database: '[my-server]pg',
+    //   entities: [],
+    //   synchronize: true,
+    // })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
